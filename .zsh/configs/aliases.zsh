5cfd603e036b61d36f56022012079bb88f0212ab
Tyler Wolf <thetylerwolf@protonmail.com>
2020-03-20 17:05:21 -0400

    change things

---
+++ .zsh/configs/aliases.zsh
@@ -0,0 +1,9 @@
+alias vimconfig="nvim ~/.vimrc"
+
+alias zshconfig="nvim ~/.zshrc"
+alias zshplugins="nvim ~/.zsh_plugins.txt"
+alias zshupdate="$ANTIBODY bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh"
+
+alias venvrun="source venv/bin/activate"
+
+alias ls="ls --color=auto"
---
+++ .zsh/configs/exports.zsh
@@ -0,0 +1,13 @@
+export EDITOR="nvim"
+export BROWSER="firefox"
+export TERM=xterm-kitty
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+export PATH=$PATH:/home/tyler/bin
+export PATH=$PATH:/home/tyler/.gem/ruby/2.7.0/bin
+export PATH=$PATH:/home/tyler/julia-1.3.1/bin
+export FZF_BASE="/usr/share/fzf"
+export NODE_ENV=development
+export PATH=$PATH:/home/tyler/Downloads/SideQuest-0.10.2
+
+export NVM_DIR="/home/tyler/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
